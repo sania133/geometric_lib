ca196382f5d9b46cc219c8b4154d250bac0cd2b4
Alexandra Smirnova <467512@niuitmo.ru>
2024-12-08 21:10:51 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,13 +1,13 @@
 import math
 
 def area(a, b, c):
-    assert a > 0 and b > 0 and c > 0, "Стороны должны быть положительными"
-    assert a + b > c and a + c > b and b + c > a, "Неверные стороны треугольника"
+    assert a > 0 and b > 0 and c > 0, ValueError
+    assert a + b > c and a + c > b and b + c > a, ValueError
     
     s = (a + b + c) / 2
     return math.sqrt(s * (s - a) * (s - b) * (s - c))
 
 def perimeter(a, b, c):
-    assert a > 0 and b > 0 and c > 0, "Стороны должны быть положительными"
+    assert a > 0 and b > 0 and c > 0, ValueError
     return a + b + c
 
