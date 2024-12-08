ebf3a513f951266919aadaacec8c22b3986bfe35
Alexandra Smirnova <467512@niuitmo.ru>
2024-12-08 23:03:37 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -6,7 +6,9 @@ def area(radius):
         raise ValueError("Радиус не может быть отрицательным")
     return math.pi * (radius ** 2)
 
+
 def perimeter(radius):
     if radius < 0:
         raise ValueError("Радиус не может быть отрицательным")
     return 2 * math.pi * radius
+    
