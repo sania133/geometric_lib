6012a022c2d1479f74b89e4f6217ec6d2eaf2be9
Alexandra Smirnova <467512@niuitmo.ru>
2024-12-08 23:56:45 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,29 +1,53 @@
 import circle
 import square
-'''Импортируем модули circle и square, которые содержат функции для вычисления площади и периметра соответствующих фигур'''
+import triangle
 
-figs = ['circle', 'square']
-'''Создает список доступных фигур'''
+figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
-'''Создает список доступных функций'''
-sizes = {}
-'''Создает словарь sizes, который будет использоваться для хранения информации о размерах фигур'''
+sizes = {
+    'circle-area': 1,
+    'circle-perimeter': 1,
+    'square-area': 1,
+    'square-perimeter': 1,
+    'triangle-area': 3,
+    'triangle-perimeter': 3
+}
+
+
 def calc(fig, func, size):
-'''Задаем функцию calc, которая принимает параметры fig - название фигуры, func - название функции, size - список значений, необходимых для вычислений, и печатает результат выполнения функции func из модуля fig'''
-    assert fig in figs
-    assert func in funcs
+    assert fig in figs, "Invalid figure"
+    assert func in funcs, "Invalid function"
+
+    key = f'{fig}-{func}'
+    args = sizes.get(key)
+    assert args is not None
+    assert len(size) == args
+
+    assert all(s >= 0 for s in size)
+
+    if fig == "triangle":
+        a, b, c = size
+        assert a + b > c and a + c > b and b + c > a
+
     result = eval(f'{fig}.{func}(*{size})')
-    print(f'{func} of {fig} is {result}')
+    return result
+
+
 if __name__ == "__main__":
-'''Этот блок кода выполняется только тогда, когда файл запущен как основная программа'''
     func = ''
     fig = ''
-    size = list()
+    size = []
+
     while fig not in figs:
-       fig = input(f"Enter figure name, avaliable are {figs}:\n")
+        fig = input(f"Enter figure name, available are {figs}:\n")
+
     while func not in funcs:
-       func = input(f"Enter function name, avaliable are {funcs}:\n")
-    while len(size) != sizes.get(f"{func}-{fig}", 1):
-       size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-'''Циклы работают, если пользователь ввел некорректное название функции или фигуры или недостаточное количество аргументов'''
-    calc(fig, func, size)
+        func = input(f"Enter function name, available are {funcs}:\n")
+
+    while len(size) != sizes.get(f"{fig}-{func}", 1):
+        size = list(map(int, input(
+            "Input figure sizes separated by space, 1 for circle and square:\n"
+        ).split()))
+
+    result = calc(fig, func, size)
+    print(f"Result: {result}")
